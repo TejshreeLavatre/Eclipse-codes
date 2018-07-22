10c026ace6441e7ca74b2170f18a5e42bc056c94
TejshreeLavatre <tj.241094@gmail.com>
2018-07-22 09:25:32 -0700

    Displaying same output using different methods

---
+++ Initial.py
@@ -0,0 +1,22 @@
+#Obtaining the same output in different ways
+
+#Using regular print statement
+print("Hello Python")
+
+
+#Using function calling
+def hello():
+    print("Hello Python")
+    
+hello()
+
+#Using object oriented programming
+class Python():
+    
+    def HELLO(self):
+        print("Hello Python")
+
+inst = Python()
+
+inst.HELLO()
+    
